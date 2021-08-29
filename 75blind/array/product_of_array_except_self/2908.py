eefcd15aaa435203b9d188cfb8fccbfc9f0473d9
German Abramov <germanjke@gmail.com>
2021-08-29 18:59:09 +0300

    Create 2908.py

---
+++ 75blind/array/product_of_array_except_self/2908.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        output = [0] * n
+        
+        p=1
+        for i in range(n):
+            output[i] = p
+            p *= nums[i]
+            
+        p=1
+        for i in range(n-1, -1, -1):
+            output[i] *= p
+            p *= nums[i]
+        
+            
+        return output
